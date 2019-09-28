2eef70a28e2def0990f52f3471184d2de5df9e36
picressant <pierre.cressant@gmail.com>
2019-09-28 12:27:27 +0200

    feat: polishing user item page

--- menus-front/src/app/main/user/pages/user-item-page/user-item-page.component.ts
+++ menus-front/src/app/main/user/pages/user-item-page/user-item-page.component.ts
@@ -6,9 +6,9 @@ import { UserRestService } from "../../../services/user-rest.service";
 import { AbstractItemPage } from "../../../../shared/components/pages/abstract-item-page";
 import { ToasterService } from "../../../../shared/services/toaster.service";
 import { Observable } from "rxjs";
-import { AddIngredientDialogComponent } from "../../../recipe/components/add-ingredient-dialog/add-ingredient-dialog.component";
 import { MatDialog } from "@angular/material/dialog";
 import { ResetPasswordDialogComponent } from "../../components/reset-password-dialog/reset-password-dialog.component";
+import { AuthService } from "../../../../shared/services/auth.service";
 
 @Component({
   selector: 'menus-user-item-page',
@@ -23,6 +23,7 @@ export class UserItemPageComponent extends AbstractItemPage<User> implements OnI
     private fb: FormBuilder,
     private route: ActivatedRoute,
     private userService: UserRestService,
+    private authService: AuthService,
     private toaster: ToasterService,
     private router: Router,
     private dialog: MatDialog,
@@ -53,4 +54,17 @@ export class UserItemPageComponent extends AbstractItemPage<User> implements OnI
     return this.userService.saveUser(this.form.value);
   }
 
+  _save() {
+    this.save$.subscribe(
+      (user: User) => {
+        this.resetForm(user);
+
+        if (this.authService.user.getValue().id === this.id) {
+          this.authService.user.next(user);
+        }
+
+        this.toaster.info(this.saveToast);
+      }
+    );
+  }
 }
