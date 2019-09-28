b7564aca88b0e47c9c787477b2f68b8804b10d8e
Pierre CRESSANT <pierre.cressant@gmail.com>
2019-09-28 13:31:09 +0200

    ci: changed deploy to C: on piotr prod

--- menus-back/src/main/resources/deploy-pcr.bat
+++ menus-back/src/main/resources/deploy-pcr.bat
@@ -1,10 +1,10 @@
-cd D:\Work\menus\menus-front
-rmdir /Q /S D:\Work\menus\menus-front\node_modules
-D:
+cd C:\work\menus\menus-front
+rmdir /Q /S C:\work\menus\menus-front\node_modules
+C:
 call npm install
 call npm run build
 
-cd D:\Work\menus\menus-back
+cd C:\work\menus\menus-back
 call mvn clean install
 
 C:\apache-tomcat-9.0.14\bin\tomcat9.exe stop
@@ -14,7 +14,7 @@ rmdir /Q /S C:\apache-tomcat-9.0.14\webapps\back
 mkdir C:\apache-tomcat-9.0.14\webapps\menus
 del /F /Q C:\apache-tomcat-9.0.14\webapps\back.war
 
-xcopy /E D:\Work\menus\menus-front\dist\menus-front-app C:\apache-tomcat-9.0.14\webapps\menus
-copy D:\Work\menus\menus-back\target\back.war C:\apache-tomcat-9.0.14\webapps\back.war
+xcopy /E C:\work\menus\menus-front\dist\menus-front-app C:\apache-tomcat-9.0.14\webapps\menus
+copy C:\work\menus\menus-back\target\back.war C:\apache-tomcat-9.0.14\webapps\back.war
 
 C:\apache-tomcat-9.0.14\bin\tomcat9.exe start
