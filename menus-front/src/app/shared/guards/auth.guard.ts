0f9da93c3438862c18fe3afae19d2473f951f2cd
picressant <pierre.cressant@gmail.com>
2019-09-28 10:57:51 +0200

    fix: migration errors

--- menus-front/src/app/shared/guards/auth.guard.ts
+++ menus-front/src/app/shared/guards/auth.guard.ts
@@ -1,37 +1,34 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router/src/utils/preactivation';
-import { Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { Observable, Observer } from "rxjs";
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
-    path: ActivatedRouteSnapshot[];
-    route: ActivatedRouteSnapshot;
+  constructor(
+    private router: Router,
+    private authenticationService: AuthService
+  ) {
+  }
 
-    constructor(
-        private router: Router,
-        private authenticationService: AuthService
-    ) { }
-
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        const token = this.authenticationService.getToken();
-        if (token) {
-          if (this.authenticationService.user.getValue() == null) {
-            return Observable.create((observer: Observer<boolean>) => {
-              this.authenticationService.loadCurrentUser()
-                .subscribe(res => {
-                  this.authenticationService.user.next(res);
-                  observer.next(true);
-                  observer.complete();
-                }, err => observer.error(err));
-            });
-          }
-          return true;
-        }
-
-        // not logged in so redirect to login page with the return url
-        this.router.navigate(['/auth/login'], { queryParams: { returnUrl: state.url } });
-        return false;
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    const token = this.authenticationService.getToken();
+    if (token) {
+      if (this.authenticationService.user.getValue() == null) {
+        return Observable.create((observer: Observer<boolean>) => {
+          this.authenticationService.loadCurrentUser()
+            .subscribe(res => {
+              this.authenticationService.user.next(res);
+              observer.next(true);
+              observer.complete();
+            }, err => observer.error(err));
+        });
+      }
+      return true;
     }
+
+    // not logged in so redirect to login page with the return url
+    this.router.navigate(['/auth/login'], { queryParams: { returnUrl: state.url } });
+    return false;
+  }
 }
