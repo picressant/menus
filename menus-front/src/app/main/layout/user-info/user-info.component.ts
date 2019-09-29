e3f946d89e82415cd4ab78d8122c21090338a090
picressant <pierre.cressant@gmail.com>
2019-09-29 11:37:13 +0200

    feat: added avatar in header bar

--- menus-front/src/app/main/layout/user-info/user-info.component.ts
+++ menus-front/src/app/main/layout/user-info/user-info.component.ts
@@ -10,17 +10,30 @@ import { AuthService } from 'src/app/shared/services/auth.service';
 export class UserInfoComponent implements OnInit {
 
   user: User;
+  timestamp: string;
 
   constructor(
     private authService: AuthService
   ) {
     this.authService.user.subscribe(
-      (u) => this.user = u
+      (u) => {
+        this.user = u;
+        this.timestamp = new Date().getTime().toString();
+      }
     );
    }
 
   ngOnInit() {
     this.user = this.authService.user.getValue();
+  }
+
+  get imgStyles() {
+    return {
+      width: '50px',
+      height: '50px',
+      'border-radius': '50%',
+      'object-fit': 'cover'
+    };
   }
 
 }
