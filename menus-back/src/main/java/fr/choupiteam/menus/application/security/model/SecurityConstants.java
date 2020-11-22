b047d66e2f2e91f3c7327a42072c2dc48e8cbbbf
picressant <pierre.cressant@gmail.com>
2020-11-22 17:34:29 +0100

    chore: token expiration set to 168h

--- menus-back/src/main/java/fr/choupiteam/menus/application/security/model/SecurityConstants.java
+++ menus-back/src/main/java/fr/choupiteam/menus/application/security/model/SecurityConstants.java
@@ -2,7 +2,7 @@ package fr.choupiteam.menus.application.security.model;
 
 public class SecurityConstants {
     public static final String SECRET = "SecretKeyToGenJWTs";
-    public static final long EXPIRATION_TIME = 1000 * 3600 * 8; // 8 hours
+    public static final long EXPIRATION_TIME = 1000 * 3600 * 168; // 8 hours
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "authorization";
     public static final String SIGN_UP_URL = "/user/sign-up";
